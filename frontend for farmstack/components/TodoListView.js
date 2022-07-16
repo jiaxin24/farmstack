de694f9b544e6fe08703c05df06daa6a7eef7a7d
Jia Xin Lum <lumjiaxin24@gmail.com>
2022-07-16 19:26:59 +0800

    added frontend stuff

---
+++ frontend for farmstack/components/TodoListView.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import TodoItem from './Todo'
+
+export default function TodoView(props) {
+    return (
+        <div>
+            <ul>
+                {props.todoList.map(todo => <TodoItem todo={todo} />)}
+            </ul>
+        </div>
+    )
+}
